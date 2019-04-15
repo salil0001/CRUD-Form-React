2249405895f41e111d4f0714e92dc5d5569cb9b3
shorupandna <34935249+salil0001@users.noreply.github.com>
2019-04-15 13:49:04 +0530

    new commoit

---
+++ src/Form/Form.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    state = {
+        names: [
+            { id: 1, name: "Salil" },
+            { id: 2, name: "Anmol" },
+
+        ],
+
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+
+
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let abc = {
+            name: this.state.name,
+            id: Math.random()
+
+        }
+        console.log(this.state.names)
+        this.setState({
+            names: [...this.state.names, abc]
+        })
+    }
+    handleDelete=(id)=>
+    {
+        let names = this.state.names.filter(name=>{
+                return name.id !==id
+        })
+        this.setState({
+            names:names
+        })
+
+    }
+    render() {
+
+        const { names } = this.state;
+        const nameList = names.map(name => {
+            return (
+                <div key={name.id}>
+                    <div>{name.name}</div>
+                    <button onClick={()=>this.handleDelete(name.id)}>Delete</button>
+                </div>
+            )
+        })
+
+        return (
+            <div className="col-md-6  offset-3">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="text" required className="form-control" name="name" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+
+                {nameList}
+
+            </div>
+
+        )
+    }
+}
